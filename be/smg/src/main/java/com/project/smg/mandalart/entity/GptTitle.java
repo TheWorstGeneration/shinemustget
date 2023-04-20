dd923ab0ec91f54b1681c35a4c14f56c145a16d6
최동호 <ehdrmfdl002@naver.com>
2023-04-20 14:47:27 +0900

    :tada: skeleton
    skeleton

---
+++ be/smg/src/main/java/com/project/smg/mandalart/entity/GptTitle.java
@@ -0,0 +1,26 @@
+package com.project.smg.mandalart.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "gpt_title")
+public class GptTitle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gpt_title_id")
+    private int id;
+
+    private String content;
+
+    @OneToMany(mappedBy = "gptTitle")
+    private List<GptBigGoal> gptBigGoals = new ArrayList<>();
+}
