99a972b220ddfb47bac69a0879ec5bfd3d3995f3
yooooonzzzzzang <cherishvalue@naver.com>
2023-04-25 08:45:19 +0900

    🎨 add PodoCreateDto

---
+++ be/smg/src/main/java/com/project/smg/podo/dto/PodoCreateDto.java
@@ -0,0 +1,11 @@
+package com.project.smg.podo.dto;
+
+import lombok.Data;
+
+@Data
+public class PodoCreateDto {
+    private int id;
+    private int stickerTypeId;
+    private String oneline;
+
+}
