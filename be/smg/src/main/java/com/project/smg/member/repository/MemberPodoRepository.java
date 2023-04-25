e5c0e9d5f96fa8b5ea983a31cd2e0a2e059da8bb
yooooonzzzzzang <cherishvalue@naver.com>
2023-04-25 22:09:17 +0900

    :package: add MemberPodoRepository

---
+++ be/smg/src/main/java/com/project/smg/member/repository/MemberPodoRepository.java
@@ -0,0 +1,8 @@
+package com.project.smg.member.repository;
+
+import com.project.smg.member.entity.MemberPodo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberPodoRepository extends JpaRepository<MemberPodo, Integer> {
+
+}
