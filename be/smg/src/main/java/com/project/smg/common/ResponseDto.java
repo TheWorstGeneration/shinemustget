eee3511a21d9e0fb2e5056578703dbbc6d517478
yooooonzzzzzang <cherishvalue@naver.com>
2023-04-25 10:47:48 +0900

    🎨 modify ResponseDto

--- be/smg/src/main/java/com/project/smg/common/ResponseDto.java
+++ be/smg/src/main/java/com/project/smg/common/ResponseDto.java
@@ -1,9 +1,11 @@
 package com.project.smg.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-public class ResponseDto<T> {
+@AllArgsConstructor
+public class ResponseDto {
     private int statusCode;
     private String message;
 
