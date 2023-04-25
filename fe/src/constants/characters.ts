84a008f1bcf9427c31a523fd54747be8ba86c55e
임성빈 <mae03087@naver.com>
2023-04-26 08:47:02 +0900

    🚚 rename characters file

---
+++ fe/src/constants/characters.ts
@@ -0,0 +1,31 @@
+interface Character {
+    name: string;
+    image: string;
+}
+
+export const CHARACTERS: Character[] = [
+    {
+        name: '오타니 쇼헤이',
+        image: 'assets/images/characters/ohtani.png'
+    },
+    {
+        name: '마이클 조던',
+        image: 'assets/images/characters/michael_jordan.png'
+    },
+    {
+        name: '워렌 버핏',
+        image: 'assets/images/characters/warren_buffett.png'
+    },
+    {
+        name: '빌 게이츠',
+        image: 'assets/images/characters/bill_gates.png'
+    },
+    {
+        name: '김연아',
+        image: 'assets/images/characters/kim_yuna.png'
+    },
+    {
+        name: '아인슈타인',
+        image: 'assets/images/characters/einstein.png'
+    },
+];
