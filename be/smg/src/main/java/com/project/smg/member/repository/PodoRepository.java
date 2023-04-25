781a31354b473c2a1e2fb80aae241de5b0c1a10a
yooooonzzzzzang <cherishvalue@naver.com>
2023-04-26 01:48:43 +0900

    :fire: delete PodoRepository.java

--- be/smg/src/main/java/com/project/smg/member/repository/PodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.smg.member.repository;
-
-import com.project.smg.podo.entity.Podo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PodoRepository extends JpaRepository<Podo, Integer> {
-}
