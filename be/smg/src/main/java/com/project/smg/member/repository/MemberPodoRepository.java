26ba4643a56d123c61a7d7f58b7f767b7b511825
yooooonzzzzzang <cherishvalue@naver.com>
2023-05-01 14:19:51 +0900

    🐛 Fix create

--- be/smg/src/main/java/com/project/smg/member/repository/MemberPodoRepository.java
+++ be/smg/src/main/java/com/project/smg/member/repository/MemberPodoRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberPodoRepository extends JpaRepository<MemberPodo, Integer> {
     // 멤버가 가진 멤버 포도의 포도타입의 아이디 리스트
     @Query("select mp from MemberPodo mp where mp.member.id =:memberId")
     List<MemberPodo> findByPodoTypeId(@Param("memberId") String id);
-    @Query("select mp from MemberPodo mp where mp.podoType.name =:stickerName")
-    MemberPodo findByName(@Param("stickerName") String stickerType);
+    @Query("select mp from MemberPodo mp where mp.podoType.id =:ptid and mp.member.id=:mid")
+    Optional<MemberPodo> findByName(@Param("ptid")int ptid, @Param("mid")String mid);
 }
