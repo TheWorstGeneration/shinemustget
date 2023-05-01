49ed58848c3bab5d18fdb8c65bfa90c234113f5a
임성빈 <mae03087@naver.com>
2023-05-01 14:50:01 +0900

    🎨 create delete mandalart button

---
+++ fe/src/components/atoms/DeleteButton/DeleteButton.tsx
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const ButtonStyle = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 45%;
+  height: 3rem;
+
+  border-radius: 0.5rem;
+  background-color: #ffffff;
+  margin: 1rem 0;
+  padding: 0.5rem 1rem;
+
+  z-index: 200;
+
+  box-shadow: 0px 0px 10px rgba(255, 0, 0, 0.1);
+
+  &:hover {
+    background-color: #fff4f4;
+    box-shadow: inset 0px 0px 10px rgba(255, 0, 0, 0.2);
+
+    & > * {
+      color: #ff0909;
+    }
+  }
+`;
+
+const ButtonText = styled.span`
+  font-size: 0.75rem;
+  font-weight: 900;
+
+  margin-left: 0.5rem;
+
+  color: #ff090988;
+`;
+
+export const DeleteButton = () => {
+  const handleDeleteMandalart = () => {};
+
+  return (
+    <ButtonStyle type="button" onClick={handleDeleteMandalart}>
+      <FontAwesomeIcon icon={faTrashCan} color="#ff090988" />
+      <ButtonText>만다라트 삭제</ButtonText>
+    </ButtonStyle>
+  );
+};
