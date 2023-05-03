6472f4a24b6c16aa942934278664fbbacc5122ef
조재형 <whwogud0301@naver.com>
2023-05-03 14:48:28 +0900

    :sparkles: add searchPage

---
+++ fe/src/pages/search/index.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+import { useInnerWidth } from '@/hooks/useInnerWidth';
+
+const SearchSection = styled.section`
+  display: flex;
+  justify-content: center;
+  margin: 6.5rem 0 0;
+`;
+
+const SearchContainer = styled.section<{ isMaxWidth: boolean }>`
+  display: flex;
+  flex-direction: column;
+
+  width: ${({ isMaxWidth }) => (isMaxWidth ? '50vw' : '100vw')};
+  padding: ${({ isMaxWidth }) => (isMaxWidth ? '0' : '0 10rem')};
+  height: 91vh;
+
+  background-color: #ffffff;
+
+  @media (max-width: 960px) {
+    padding: 0;
+  }
+
+  @media (max-width: 500px) {
+    height: 130vh;
+  }
+`;
+
+const Search = () => {
+  const isMaxWidth = useInnerWidth() >= 1440;
+
+  return (
+    <SearchSection>
+      <SearchContainer isMaxWidth={isMaxWidth}></SearchContainer>
+    </SearchSection>
+  );
+};
+
+export default Search;
