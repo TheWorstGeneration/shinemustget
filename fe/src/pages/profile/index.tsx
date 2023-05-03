96ebf62ec738ff4e78817791fbdc202a1b0c114b
임성빈 <mae03087@naver.com>
2023-05-03 13:16:45 +0900

    🔍 add title and meta tag to improve seo

--- fe/src/pages/profile/index.tsx
+++ fe/src/pages/profile/index.tsx
@@ -3,6 +3,7 @@ import { ProfileProgress } from '@/components/molecules/ProfileProgress/ProfileP
 import styled from '@emotion/styled';
 import { MailContainer } from '@/components/molecules/MailContainer/MailContrainer';
 import { useInnerWidth } from '@/hooks/useInnerWidth';
+import Head from 'next/head';
 
 const Temp = styled.section`
   display: flex;
@@ -37,15 +38,36 @@ export const Profile = () => {
   const isMaxWidth = useInnerWidth() >= 1440;
 
   return (
-    <Temp>
-      <ProfileSection isMaxWidth={isMaxWidth}>
-        <ProfileProgress />
-        <ProfileLog />
-      </ProfileSection>
-      <Mail>
-        <MailContainer />
-      </Mail>
-    </Temp>
+    <>
+      <Head>
+        <title>Shine Must Get | 개인 프로필 관리</title>
+        <meta
+          name="description"
+          content="프로필 페이지에서 현재 만다라트 진척도를 확인하고, 지난 만다라트를 확인할 수 있습니다."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Shine Must Get" />
+        <meta property="og:title" content="Shine Must Get | 개인 프로필 관리" />
+        <meta
+          property="og:description"
+          content="프로필 페이지에서 현재 만다라트 진척도를 확인하고, 지난 만다라트를 확인할 수 있습니다."
+        />
+        <meta
+          property="og:image"
+          content="assets/images/common/front-image.png"
+        />
+        <meta property="og:url" content="https://shinemustget.com" />
+      </Head>
+      <Temp>
+        <ProfileSection isMaxWidth={isMaxWidth}>
+          <ProfileProgress />
+          <ProfileLog />
+        </ProfileSection>
+        <Mail>
+          <MailContainer />
+        </Mail>
+      </Temp>
+    </>
   );
 };
 
