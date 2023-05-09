ea4c055b7b04902d83b162f4595a58bc3086e618
조재형 <whwogud0301@naver.com>
2023-05-10 01:21:54 +0900

    Update SecurityConfig disable cache

--- be/smg/src/main/java/com/project/smg/auth/config/SecurityConfig.java
+++ be/smg/src/main/java/com/project/smg/auth/config/SecurityConfig.java
@@ -47,7 +47,9 @@ public class SecurityConfig {
                 .formLogin().disable() // FormLogin 사용 X
                 .httpBasic().disable() // httpBasic 사용 X
                 .csrf().disable() // csrf 보안 사용 X
-                .headers().frameOptions().disable()
+                .headers()
+                    .frameOptions().disable()
+                    .cacheControl().disable() // 캐시 비활성화
                 .and()
 
                 // 세션 사용하지 않으므로 STATELESS로 설정
