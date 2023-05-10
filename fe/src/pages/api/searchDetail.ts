af6e806b70e50a92c1f8be3067e55413c9b55825
김영주 <eorhrtnv@naver.com>
2023-05-10 17:32:02 +0900

    Update DetailPage

--- fe/src/pages/api/searchDetail.ts
+++ fe/src/pages/api/searchDetail.ts
@@ -1,9 +1,10 @@
+import { MANDALART_SEARCH_DETAIL } from "@/constants/queryKey";
 import axios from "axios";
 
 interface podoId { id: number }
 
 const searchDetail = async (props:number) => {
-  const data = await axios.post<podoId>(`https://shinemustget.com/api/podo/setting/${props}`, {}, {
+  const data = await axios.post<podoId>(MANDALART_SEARCH_DETAIL+`${props}`, {}, {
     headers: {
       id: 2762543073,
     },
