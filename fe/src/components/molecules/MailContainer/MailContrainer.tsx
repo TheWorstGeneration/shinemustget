69cce812f1bf2a3af0f4cfb149006fc04213e182
김영주 <eorhrtnv@naver.com>
2023-05-15 17:26:52 +0900

    Update mailContainer

--- fe/src/components/molecules/MailContainer/MailContrainer.tsx
+++ fe/src/components/molecules/MailContainer/MailContrainer.tsx
@@ -117,12 +117,11 @@ export function MailContainer() {
     //TODO: mail controller에서 메일을 받아와서 알림창에 띄우기
     // console.log('메일 받아오기');
 
-    if (socket) { 
       socket.onmessage = (event) => { 
         const message = event.data;
-        console.log("앙 메세지띠", message.message);
-      }
+        console.log("앙 메세지띠", message);
     }
+    
   }, [socket]);
 
   return isLandingPage ? null : (
