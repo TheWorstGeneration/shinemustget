4fdd2195589d49a7dadeab8564146adca4090476
김영주 <eorhrtnv@naver.com>
2023-05-17 15:37:09 +0900

    Update mailContainer

--- fe/src/components/atoms/MailBox/MailBox.tsx
+++ fe/src/components/atoms/MailBox/MailBox.tsx
@@ -7,25 +7,50 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Dispatch, SetStateAction } from 'react';
 
-const Mail = styled.article`
+const MailContainer = styled.div`
+  border-radius: 0.5rem;
+  background-color: #ffffff;
+  box-shadow: inset 0px 0px 5px rgba(0, 0, 0, 0.1);
+
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
   width: 100%;
   margin: 0.5rem 0;
+  height:7rem;
+`;
 
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
-  background-color: #ffffff;
-  box-shadow: inset 0px 0px 5px rgba(0, 0, 0, 0.1);
+const Mail = styled.article`
+
+  padding: 0.5rem 0 .5rem 1rem ;
+ 
+`;
+
+
+const MailText = styled.div`
+
+`;
+
+const MailDate = styled.div`
+  margin-top: 0.5rem;
+  text-align: right;
+  font-size: 0.9rem;
 `;
 
 const MailFooter = styled.footer`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  margin-left: 1vw;
+  height: 100%;
+  padding: 1rem;
+  border-radius: 0 1rem 1rem 0;
+  &:hover{
+    background-color: #FAFAFA;
+  }
 `;
+
 
 export const MailBox = ({
   key,
@@ -41,14 +66,16 @@ export const MailBox = ({
   };
 
   return (
-    <Mail>
-      <p>{mail.message}</p>
-      <p>{mail.formattedCreatedAt}</p>
+    <MailContainer>
+      <Mail>
+        <MailText>{mail.message}</MailText>
+        <MailDate>{mail.formattedCreatedAt}</MailDate>
+      </Mail>
       <MailFooter>
         <button type="button" title="확인">
           <FontAwesomeIcon icon={faCheck} onClick={handleOnClick} />
         </button>
       </MailFooter>
-    </Mail>
+    </MailContainer>
   );
 };
