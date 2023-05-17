6b0c0fc79ef205de37887c42ca9d17691991225a
김영주 <eorhrtnv@naver.com>
2023-05-17 10:32:47 +0900

    update mailContainer

--- fe/src/hooks/useSocket.ts
+++ fe/src/hooks/useSocket.ts
@@ -1,5 +1,9 @@
 export const useSocket = () => {
   const socket = new WebSocket('wss://www.shinemustget.com/api/ws');
   
+  socket.addEventListener("close", () => {
+    console.log("Disconnect")
+  })
+
   return socket
 };
